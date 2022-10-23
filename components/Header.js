42a193bbc711057bfcfd20b3965227ae7c2e6ee2
viktor <viktor.saprykin@warwick.ac.uk>
2022-10-23 19:19:04 +0100

    scroll to top on render fix for mobile test 8

--- components/Header.js
+++ components/Header.js
@@ -108,7 +108,22 @@ export default function Header() {
                 </Box>
             </Flex>
 
-
+            <Box
+                className="custom-shape-divider-top-1664755733"
+                bgGradient={"linear(to-r, #7C82FF,teal.400 )"}
+                position={"relative"}
+                zIndex={"2"}
+                mb={"-70vh"}
+            >
+                <svg
+                    data-name="Layer 1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1200 120"
+                    preserveAspectRatio="none"
+                >
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
+                </svg>
+            </Box>
         </Box>
     )
 }
